5f15f301273c2d52faf0c1e7edb5a8253861a026
Mr Singh <ashishs723@gmail.com>
2025-04-05 11:05:54 +0530

    materail 15 update

--- src/app/shared/components/navbar/navbar.module.ts
+++ src/app/shared/components/navbar/navbar.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
